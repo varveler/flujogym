e6f071cd6c717147d26cb32ee01fbad5ddee268d
varveler <evarval@gmail.com>
2025-02-10 20:15:02 -0600

    admin para ambos modelos

--- flujogym/rutinas/admin.py
+++ flujogym/rutinas/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Ejercicio, Rutina, EjercicioEnRutina, ProgramacionRutina
 
-# Register your models here.
+
+@admin.register(Ejercicio)
+class EjercicioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'grupo_muscular', 'equipamiento_necesario')
+    list_filter = ('grupo_muscular',)
+    search_fields = ('nombre', 'descripcion')
+
+
+class EjercicioEnRutinaInline(admin.TabularInline):
+    model = EjercicioEnRutina
+    extra = 1
+    ordering = ('orden',)
+
+
+@admin.register(Rutina)
+class RutinaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'nivel_dificultad', 'duracion_estimada', 'creada_por', 'fecha_creacion')
+    list_filter = ('nivel_dificultad', 'fecha_creacion')
+    search_fields = ('nombre', 'descripcion', 'creada_por__username')
+    inlines = [EjercicioEnRutinaInline]
+    readonly_fields = ('fecha_creacion',)
+
+
+@admin.register(ProgramacionRutina)
+class ProgramacionRutinaAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'rutina', 'dia_semana', 'hora', 'activa')
+    list_filter = ('dia_semana', 'activa', 'usuario')
+    search_fields = ('usuario__username', 'rutina__nombre')
+    raw_id_fields = ('usuario', 'rutina')
+
+    def get_queryset(self, request):
+        # Optimizacion con select_related
+        return super().get_queryset(request).select_related('usuario', 'rutina')
+
+
+@admin.register(EjercicioEnRutina)
+class EjercicioEnRutinaAdmin(admin.ModelAdmin):
+    list_display = ('rutina', 'ejercicio', 'orden', 'series', 'repeticiones')
+    list_filter = ('rutina', 'ejercicio')
+    search_fields = ('rutina__nombre', 'ejercicio__nombre')
+    ordering = ('rutina', 'orden')
+
+
+
+
--- flujogym/usuarios/admin.py
+++ flujogym/usuarios/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'activo', 'fecha_inscripcion')
+    list_filter = ('activo', 'fecha_inscripcion', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('-fecha_inscripcion',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Información Personal', {'fields': (
+            'first_name',
+            'last_name',
+            'email',
+            'fecha_nacimiento',
+            'telefono',
+            'direccion',
+        )}),
+        ('Estado', {'fields': (
+            'activo',
+            'fecha_inscripcion',
+        )}),
+        ('Permisos', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions',
+        )}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+    readonly_fields = ('fecha_inscripcion',)
